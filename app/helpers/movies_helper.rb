9d65f19c08d796313047174f381fc7b1e82e5edf
cspmosh <cs.pmosh@gmail.com>
2020-10-26 09:40:06 -0500

    added some view helper methods for the movies pages

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,9 @@
 module MoviesHelper
+  def gross(movie)
+    movie.flop? ? "Flop!" : number_to_currency(movie.total_gross, precision: 0)
+  end
+
+  def year_of(movie)
+    movie.released_on&.year
+  end
 end
