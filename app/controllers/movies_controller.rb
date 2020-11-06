9b232ef4a62557466a647b64494c1ff75f6820c6
JTurner <cs.pmosh@gmail.com>
2020-11-06 14:13:22 -0600

    added a patch route and an update action which allows the form on the edit page to update a movie record in the database

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,4 +10,12 @@ class MoviesController < ApplicationController
   def edit
     @movie = Movie.find(params[:id])
   end
+
+  def update
+    @movie = Movie.find(params[:id])
+    params_allowed = params.require(:movie)
+      .permit(:name, :description, :rating, :released_on, :total_gross)
+    Movie.update(params_allowed)
+    redirect_to @movie
+  end
 end
