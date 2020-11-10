aca62fd20fda77a73e13c4ef0a86d0c2dd631e4e
JTurner <cs.pmosh@gmail.com>
2020-11-10 14:38:40 -0600

    added validations to Movie model and changed the rating on the form to a drop down input

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,16 @@
 class Movie < ApplicationRecord
+
+  RATINGS = %w(G PG PG-13 R NC-17)
+
+  validates :name, :released_on, :duration, presence: true
+  validates :description, length: { minimum: 25 }
+  validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
+  validates :image_file_name, format: {
+    with: /\w+\.(png|jpg)\z/i,
+    message: "must be a JPG or a PNG image"
+  }
+  validates :rating, inclusion: { in: RATINGS }
+
   def self.released
     Movie.where("released_on <= ?", Time.now).order("released_on desc")
   end
