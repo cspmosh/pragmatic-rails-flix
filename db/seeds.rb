0af27a260668d5280a4ee8ec362b4e05c9d5dbd2
cspmosh <cs.pmosh@gmail.com>
2020-11-10 03:01:47 -0600

    added 'has_many' relationship from movie to reviews, added some reviews to the seeds file, and added some validation to the review model

--- db/seeds.rb
+++ db/seeds.rb
@@ -187,3 +187,9 @@ Movie.create!([
     image_file_name: "placeholder.png"
   }
 ])
+
+movie = Movie.find_by(name: "Captain Marvel")
+
+movie.reviews.create!(name: "Larry", stars: 5, comment: "Awesome!")
+movie.reviews.create!(name: "Daisy", stars: 4, comment: "Great!")
+movie.reviews.create!(name: "Moe", stars: 3, comment: "Spilled my popcorn!")
