e610d920ccdf5b94e26e3fc878c79d0b56af643c
JTurner <cs.pmosh@gmail.com>
2020-11-20 14:07:08 -0600

    added crud actions for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,51 @@
 class ReviewsController < ApplicationController
+  before_action :get_movie
+  
   def index
-    @movie = Movie.find(params[:movie_id])
     @reviews = @movie.reviews
   end
 
   def new
-    @movie = Movie.find(params[:movie_id])
     @review = @movie.reviews.new
+  end
+
+  def create
+    @review = @movie.reviews.new(review_params)
+
+    if @review.save
+      redirect_to movie_reviews_url(@movie), notice: "Thanks for your review!"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @review = @movie.reviews.find(params[:id])
+  end
+
+  def update
+    @review = @movie.reviews.find(params[:id])
+    
+    if @review.update(review_params)
+      redirect_to movie_reviews_url(@movie), notice: "Review updated!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review = @movie.reviews.find(params[:id])
+    redirect_to movie_reviews_path(params[:movie_id]), danger: "Review successfully deleted" if @review.destroy
+  end
+
+  private
+
+  def review_params 
+    params.require(:review)
+      .permit(:name, :stars, :comment)
+  end
+
+  def get_movie
+    @movie = Movie.find(params[:movie_id])
   end
 end
