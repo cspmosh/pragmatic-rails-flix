ee7d07d1ef3bb8f3c9e932dd27b96d6e3ba58a89
JTurner <cs.pmosh@gmail.com>
2020-12-29 14:47:58 -0600

    lesson 28: added ability to edit and delete user accounts. Also added a username to the user table.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,10 +20,29 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: "Account successfully updated!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to movies_path, alert: "Account successfully deleted!"
+  end
+
 private
 
   def user_params
     params.require(:user)
-      .permit(:name, :email, :password, :password_confirmation)
+      .permit(:username, :name, :email, :password, :password_confirmation)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_21_132823) do
+ActiveRecord::Schema.define(version: 2020_12_29_201348) do
 
   create_table "movies", force: :cascade do |t|
     t.string "name"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2020_12_21_132823) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "reviews", "movies"
