105754848f9b17888784ca67cde0ee9bfe9d6c7d
cspmosh <cs.pmosh@gmail.com>
2021-01-09 09:08:53 -0600

    lesson-36: added profile images to fans on the movie page.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
-  def profile_image(user)
+  def profile_image(user, size: 150)
     url = "http://secure.gravatar.com/avatar/#{user.gravatar_id}"
-    image_tag(url, alt: user.name)
+    image_tag(url, size: size, alt: user.name)
   end
 end
