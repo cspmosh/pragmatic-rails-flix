06fb7e1cb32bfb675f6ff47dae2e2912e4a56aec
cspmosh <cs.pmosh@gmail.com>
2021-01-10 18:47:20 -0600

    fixed syntax bug with critics through association and added a create action on the favorites controller to allow a signed in user to favorite a movie.

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,12 @@
 class FavoritesController < ApplicationController
+
+  before_action :require_signin
+
+  def create 
+    @movie = Movie.find(params[:movie_id])
+    @movie.favorites.new(user: current_user)
+    if @movie.save
+      redirect_to @movie
+    end
+  end
 end
